ec1bf999d621e30b9271af04e20967d687ac78b2
cms_huyuhui <243818817@qq.com>
2016-11-09 16:56:05 +0800

    例子：统计重复行duplate，生成gif文件lissajous

---
+++ duplate.go
@@ -0,0 +1,25 @@
+// duplate.go prints the test of each line that appears more than once in the standard input , preceded by ist count
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		if len(input.Text()) == 0 {
+			break
+		}
+		counts[input.Text()]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
---
+++ duplate2.go
@@ -0,0 +1,42 @@
+// duplate.go prints the test of each line that appears more than once in the standard input.
+// It read from stdin or from a list of named files
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "duplate2:  %v \n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for str, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s", n, str)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		if len(input.Text()) == 0 {
+			break
+		}
+		counts[input.Text()]++
+	}
+}
